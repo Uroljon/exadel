00df6c3fab94088936a0095f120380120c8e0729
Uroljon <uroljonkhidirboev@gmail.com>
2022-03-02 18:27:54 +0500

    clear

--- homework_2/pinCodeGenerator.js
+++
@@ -1,8 +0,0 @@
-function pinCodeGenerator(length) {
-    let rand = ""
-    for (let i = 0; i < length; i++) {
-        rand += rand ? Math.floor(Math.random()*10) : Math.ceil(Math.random()*9)
-    }
-    return Number(rand)
-}
-console.log(pinCodeGenerator(5))
--- homework_2/validatePhoneNymber.js
+++
@@ -1,5 +0,0 @@
-function validatePhoneNymber(num) {
-    num = num.split(" ").join("")
-    return Boolean(Number(num))
-}
-console.log(validatePhoneNymber("067 734 4343"))
